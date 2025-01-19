7ad724810d3231ee127849565d0b0abadccad0a3
lokeshjatoth <lokeshjatothit@gmail.com>
2025-01-19 22:11:03 +0530

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.ORIGIN_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     credentials: true
 }));
 
